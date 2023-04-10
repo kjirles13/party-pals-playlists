cb71d5b983b0749a070434c85be023843c36b118
Kit Jirles <kjirles13@gmail.com>
2023-04-10 11:29:09 -0400

    Complete SongDto model

--- java/src/main/java/com/techelevator/model/SongDto.java
+++ java/src/main/java/com/techelevator/model/SongDto.java
@@ -7,4 +7,45 @@ public class SongDto {
     private List<Genre> genres;
     private int rating;
 
+    public SongDto() {
+    }
+
+    public SongDto(int id, List<Genre> genres, int rating) {
+        this.id = id;
+        this.genres = genres;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "SongDto{" +
+                "id=" + id +
+                ", genres=" + genres +
+                ", rating=" + rating +
+                '}';
+    }
 }
