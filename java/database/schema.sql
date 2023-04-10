82f8692f5f659e5e222e709c0258b108502298fe
Kit Jirles <kjirles13@gmail.com>
2023-04-10 16:03:56 -0400

    Reorganize database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,21 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS user_role, genre_song, playlist_genre, 
+artist_song, playlist_song, dj_song, songs, genres, playlists, 
+artist, host_event, events, roles, users CASCADE;
+
+CREATE TABLE users (
+	user_id SERIAL,
+	username varchar(50) NOT NULL UNIQUE,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE roles (
+	role_id SERIAL PRIMARY KEY,
+	role_name VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE user_role (
 user_id int NOT NULL,
@@ -10,30 +25,11 @@ FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
-
-DROP TABLE IF EXISTS roles;
-
-CREATE TABLE roles (
-role_id SERIAL PRIMARY KEY,
-role_name VARCHAR(50) NOT NULL
-);
-
-DROP TABLE IF EXISTS playlists;
-
 CREATE TABLE playlists(
 id int NOT NULL PRIMARY KEY,
 name char(100) NOT NULL,
 description varchar(200)
 );
-
-DROP TABLE IF EXISTS genres;
-
-CREATE TABLE genres (
-genre_id int NOT NULL PRIMARY KEY,
-name char(50) NOT NULL
-);
-
-DROP TABLE IF EXISTS songs;
 
 CREATE TABLE songs (
 	song_id int NOT NULL PRIMARY KEY,
@@ -41,14 +37,16 @@ CREATE TABLE songs (
 	spotify_link char(250),
 	preview char(250)
 );
-DROP TABLE IF EXISTS artist;
 
 CREATE TABLE artist (
 artist_id int NOT NULL PRIMARY KEY,
 name char(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS artist_song;
+CREATE TABLE genres (
+genre_id int NOT NULL PRIMARY KEY,
+name char(50) NOT NULL
+);
 
 CREATE TABLE artist_song (
 artist_id int NOT NULL,
@@ -58,7 +56,6 @@ FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
-DROP TABLE IF EXISTS events;
 CREATE TABLE events (
 event_id int NOT NULL PRIMARY KEY,
 user_id int NOT NULL,
@@ -74,8 +71,6 @@ FOREIGN KEY (role_id) REFERENCES roles(role_id),
 FOREIGN KEY (playlist_id) REFERENCES playlists (id)
 );
 
-DROP TABLE IF EXISTS host_event;
-
 CREATE TABLE host_event (
 user_id int NOT NULL,
 event_id int NOT NULL,
@@ -83,8 +78,6 @@ PRIMARY KEY (user_id, event_id),
 FOREIGN KEY (user_id) REFERENCES users (user_id),
 FOREIGN KEY (event_id) REFERENCES events (event_id)
 );
-
-DROP TABLE IF EXISTS playlist_song;
 
 CREATE TABLE playlist_song (
 playlist_id int NOT NULL,
@@ -95,7 +88,6 @@ FOREIGN KEY (playlist_id) REFERENCES playlists (id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
-DROP TABLE IF EXISTS dj_song;
 CREATE TABLE dj_song (
 song_id int NOT NULL,
 user_id int NOT NULL,
@@ -105,8 +97,6 @@ FOREIGN KEY (song_id) REFERENCES songs (song_id),
 FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
-DROP TABLE IF EXISTS genre_song;
-
 CREATE TABLE genre_song (
 song_id int NOT NULL,
 genre_id int NOT NULL,
@@ -115,24 +105,12 @@ FOREIGN KEY (song_id) REFERENCES songs (song_id),
 FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
 );
 
-DROP TABLE IF EXISTS playlist_genre;
-
 CREATE TABLE playlist_genre (
 playlist_id int NOT NULL,
 genre_id int NOT NULL,
 PRIMARY KEY (playlist_id, genre_id),
 FOREIGN KEY (playlist_id) REFERENCES playlists (id),
 FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
-);
-
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
 COMMIT TRANSACTION;
