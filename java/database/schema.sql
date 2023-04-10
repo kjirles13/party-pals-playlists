58ec066f44acd70e4abebda09c12a994c558f6c0
Kit Jirles <kjirles13@gmail.com>
2023-04-10 16:39:42 -0400

    Add test data to database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
 
 CREATE TABLE roles (
 	role_id SERIAL PRIMARY KEY,
-	role_name VARCHAR(50) NOT NULL
+	role_name varchar(50) NOT NULL
 );
 
 CREATE TABLE user_role (
@@ -26,49 +26,50 @@ FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 CREATE TABLE playlists(
-id int NOT NULL PRIMARY KEY,
-name char(100) NOT NULL,
-description varchar(200)
+playlist_id SERIAL PRIMARY KEY,
+name varchar(100) NOT NULL,
+description varchar(200),
+spotify_id varchar(50)
 );
 
 CREATE TABLE songs (
-	song_id int NOT NULL PRIMARY KEY,
-	title char(50) NOT NULL,
-	spotify_link char(250),
-	preview char(250)
+	song_id varchar(50) NOT NULL PRIMARY KEY,
+	title varchar(50) NOT NULL,
+	spotify_link varchar(250),
+	preview varchar(250)
 );
 
 CREATE TABLE artist (
-artist_id int NOT NULL PRIMARY KEY,
-name char(50) NOT NULL
+artist_id varchar(50) NOT NULL PRIMARY KEY,
+name varchar(50) NOT NULL
 );
 
 CREATE TABLE genres (
-genre_id int NOT NULL PRIMARY KEY,
-name char(50) NOT NULL
+genre_id SERIAL PRIMARY KEY,
+name varchar(50) NOT NULL
 );
 
 CREATE TABLE artist_song (
-artist_id int NOT NULL,
-song_id int NOT NULL,
+artist_id varchar(50) NOT NULL,
+song_id varchar(50) NOT NULL,
 PRIMARY KEY (artist_id, song_id),
 FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
 CREATE TABLE events (
-event_id int NOT NULL PRIMARY KEY,
+event_id SERIAL PRIMARY KEY,
 user_id int NOT NULL,
 role_id int NOT NULL,
 from_date DATE NOT NULL,
 to_date DATE NOT NULL,
-description char(250) NOT NULL,
+description varchar(250) NOT NULL,
 playlist_id int NOT NULL,
 date_time TIMESTAMP NOT NULL,
-theme char(100),
+theme varchar(100),
 FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (role_id) REFERENCES roles(role_id),
-FOREIGN KEY (playlist_id) REFERENCES playlists (id)
+FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id)
 );
 
 CREATE TABLE host_event (
@@ -81,15 +82,15 @@ FOREIGN KEY (event_id) REFERENCES events (event_id)
 
 CREATE TABLE playlist_song (
 playlist_id int NOT NULL,
-song_id int NOT NULL,
+song_id varchar(50) NOT NULL,
 votes int NOT NULL,
 PRIMARY KEY (playlist_id, song_id),
-FOREIGN KEY (playlist_id) REFERENCES playlists (id),
+FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
 CREATE TABLE dj_song (
-song_id int NOT NULL,
+song_id varchar(50) NOT NULL,
 user_id int NOT NULL,
 song_rating int NOT NULL,
 PRIMARY KEY (song_id, user_id),
@@ -98,7 +99,7 @@ FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 CREATE TABLE genre_song (
-song_id int NOT NULL,
+song_id varchar(50) NOT NULL,
 genre_id int NOT NULL,
 PRIMARY KEY (song_id, genre_id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id),
@@ -109,7 +110,7 @@ CREATE TABLE playlist_genre (
 playlist_id int NOT NULL,
 genre_id int NOT NULL,
 PRIMARY KEY (playlist_id, genre_id),
-FOREIGN KEY (playlist_id) REFERENCES playlists (id),
+FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id),
 FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
 );
 
