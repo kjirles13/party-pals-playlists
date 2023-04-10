f8da143d68bac2fe29c175c96e0b6b173c944e08
daniel pattison <danielpattison93@gmail.com>
2023-04-10 17:03:09 -0400

    working on song jdbc

---
+++ java/src/main/java/com/techelevator/dao/JdbcSongDao.java
@@ -0,0 +1,72 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcSongDao implements SongDao {
+
+    private JdbcTemplate jdbcTemplate;
+    Song song = new Song();
+    Playlist playlist = new Playlist();
+
+    public JdbcSongDao() {
+    }
+
+    public JdbcSongDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Song> getAllSongs(Principal principal) {
+
+        List<Song> allSongs = new ArrayList<>();
+        
+
+
+
+        return null;
+    }
+
+    @Override
+    public Song addSong(String username, Song song) {
+        return null;
+    }
+
+    @Override
+    public void updateSong(SongDto songDto) {
+
+    }
+
+    @Override
+    public void deleteSong(int songId, Principal principal) {
+
+    }
+
+    private Song mapRowToSong(SqlRowSet rs) {
+        Song song = new Song();
+
+        song.setId(rs.getInt("song_id"));
+        song.setName(rs.getString("title"));
+        //song.setArtists(rs.getObject("artist"));
+
+//        private int id;
+//        private String name;
+//        private List<Artist> artists;
+//        private List<Genre> genres;
+//        private int rating;
+//        private int votes;
+//        private String preview;
+//        private String spotifyUri;
+
+
+
+        return song;
+    }
+}
