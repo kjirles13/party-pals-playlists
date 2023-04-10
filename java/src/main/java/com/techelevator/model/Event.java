35060ca3c7a8bc9eead132015125bc312408746e
Kit Jirles <kjirles13@gmail.com>
2023-04-10 11:25:51 -0400

    Create SongDto and UserDto

--- java/src/main/java/com/techelevator/model/Event.java
+++ java/src/main/java/com/techelevator/model/Event.java
@@ -1,4 +1,15 @@
 package com.techelevator.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Event {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime dateTime;
+    private Playlist playlist;
+    private List<Host> hosts;
+
+
 }
---
+++ java/src/main/java/com/techelevator/model/SongDto.java
@@ -0,0 +1,10 @@
+package com.techelevator.model;
+
+import java.util.List;
+
+public class SongDto {
+    private int id;
+    private List<Genre> genres;
+    private int rating;
+
+}
