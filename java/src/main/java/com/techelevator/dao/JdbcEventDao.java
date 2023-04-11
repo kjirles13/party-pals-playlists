0956caffa5958db4ba84c19c781ef3563c2f7be4
daniel pattison <danielpattison93@gmail.com>
2023-04-11 13:50:42 -0400

    still working on event jdbc

--- java/src/main/java/com/techelevator/dao/JdbcEventDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcEventDao.java
@@ -2,9 +2,12 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Event;
 import com.techelevator.model.Host;
+import com.techelevator.model.Playlist;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +24,7 @@ public class JdbcEventDao implements EventDao {
     public List<Event> getAllEvents(int userId) {
         List<Event> allEvents = new ArrayList<>();
         String sql = "SELECT event_id, dj_id, description, playlist_id, date_time, theme\n" +
-                "\tFROM public.events";
+                "\tFROM public.events where dj_id = ?";
 
 
         return null;
@@ -56,4 +59,20 @@ public class JdbcEventDao implements EventDao {
     public void deleteHostFromEvent(int eventId, Host host) {
 
     }
+
+    private Event mapRowToEvent(SqlRowSet rs){
+        Event event = new Event();
+
+        event.setId(rs.getInt("event_id"));
+        event.setName(rs.getString("event_name"));
+        event.setDescription(rs.getString("description"));
+        event.setDateTime(rs.getTimestamp("date_time"));
+        event.setPlaylistId(rs.getInt("playlist_id"));
+        event.setTheme(rs.getString("theme"));
+        return event;
+    }
+
+
+    
+
 }
