be6da7cafe1ba89d8ef4c3ffe2ff82115b78888d
Kit Jirles <kjirles13@gmail.com>
2023-04-12 17:23:28 -0400

    Implement getPlaylistById method

--- java/src/main/java/com/techelevator/dao/JdbcPlaylistDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPlaylistDao.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -24,52 +25,119 @@ public class JdbcPlaylistDao implements PlaylistDao{
 
     @Override
     public Playlist getPlayListById(int playlistId, int userId) {
-
         Playlist playlist = new Playlist();
-        String sql = "SELECT playlist_id, name, description, spotify_id\n" +
-                "\tFROM public.playlists\n" +
-                "\tWHERE playlist_id = ?";
+
+        String sql = "SELECT playlist_id, name, description, spotify_id " +
+                "FROM public.playlists " +
+                "WHERE playlist_id = ?";
 
         SqlRowSet playlistResults = jdbcTemplate.queryForRowSet(sql, playlistId);
 
-        if (playlistResults.next()){
+        if (playlistResults.next()) {
             playlist = mapRowToPlaylist(playlistResults);
-
-            String artistSql = "SELECT artists.artist_id, artists.name " +
-                    "FROM public.artists " +
-                    "JOIN artist_song ON artist_song.artist_id = artists.artist_id " +
-                    "WHERE artist_song.song_id = ?";
-
         }
 
-        return null;
+        String sqlSong = "SELECT song_id " +
+                "FROM playlist_song " +
+                "WHERE playlist_id = ?";
+
+        SqlRowSet songIdResults = jdbcTemplate.queryForRowSet(sqlSong, playlistId);
+
+        List<Song> songs = new ArrayList<>();
+
+        while (songIdResults.next()) {
+            String songId = songIdResults.getString("song_id");
+
+            String songSql = "SELECT songs.song_id, songs.title, songs.spotify_link, songs.preview, playlist_song.votes, dj_song.song_rating " +
+                    "FROM songs " +
+                    "LEFT JOIN dj_song ON songs.song_id = dj_song.song_id " +
+                    "LEFT JOIN playlist_song ON songs.song_id = playlist_song.song_id " +
+                    "WHERE songs.song_id = ? " +
+                    "AND playlist_song.playlist_id = ? ";
+
+            SqlRowSet songResults = jdbcTemplate.queryForRowSet(songSql, songId, playlistId);
+
+            Song song = new Song();
+
+            if (songResults.next()) {
+                song = mapRowToSong(songResults);
+
+                String artistSql = "SELECT artists.artist_id, artists.name " +
+                        "FROM public.artists " +
+                        "JOIN artist_song ON artist_song.artist_id = artists.artist_id " +
+                        "WHERE artist_song.song_id = ?";
+
+                SqlRowSet artistResults = jdbcTemplate.queryForRowSet(artistSql, songId);
+
+                List<Artist> artists = new ArrayList<>();
+
+                while (artistResults.next()) {
+                    Artist artist = mapRowToArtist(artistResults);
+
+                    artists.add(artist);
+                }
+
+                song.setArtists(artists);
+
+                String genreSql = "SELECT genres.genre_id, genres.name " +
+                        "FROM public.genres " +
+                        "JOIN genre_song ON genre_song.genre_id = genres.genre_id " +
+                        "WHERE genre_song.song_id = ?";
+
+                SqlRowSet genreResults = jdbcTemplate.queryForRowSet(genreSql, songId);
+
+                List<Genre> genres = new ArrayList<>();
+
+                while (genreResults.next()) {
+                    Genre genre = mapRowToGenre(genreResults);
+
+                    genres.add(genre);
+                }
+
+                song.setGenres(genres);
+                songs.add(song);
+            }
+        }
+
+        playlist.setSongs(songs);
+
+        String sqlGenres = "SELECT genres.genre_id, genres.name " +
+                "FROM playlist_genre " +
+                "JOIN genres ON genres.genre_id = playlist_genre.genre_id " +
+                "WHERE playlist_genre.playlist_id = ?";
+
+        SqlRowSet genreResults = jdbcTemplate.queryForRowSet(sqlGenres, playlist.getPlaylistId());
+
+        List<Genre> genres = new ArrayList<>();
+
+        while (genreResults.next()) {
+            genres.add(mapRowToGenre(genreResults));
+        }
+
+        playlist.setGenres(genres);
+
+        return playlist;
     }
 
     @Override
     public void addSongToPlaylist(int playlistId, String songId, int userId) {
-
-
         if (verifyUser(userId, playlistId)) {
-
 
             String sql = "INSERT INTO public.playlist_song(\n" +
                     "\tplaylist_id, song_id, votes)\n" +
                     "\tVALUES (?, ?, ?)";
 
             jdbcTemplate.update(sql, playlistId, songId, 0);
-
         }
         else {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
                     "You are unauthorized to modify this playlist");
         }
 
-
     }
 
     @Override
     public void deleteSongFromPlaylist(int playlistId, String songId, int userId) {
-
         if (verifyUser(userId, playlistId)){
 
             String sql = "DELETE FROM public.playlist_song\n" +
@@ -82,11 +150,10 @@ public class JdbcPlaylistDao implements PlaylistDao{
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
                     "You are unauthorized to modify this playlist");
         }
-
     }
 
     @Override
-    public void updateVotesForSong(int playlistId, int votes, int userId) {
+    public void updateVotesForSong(int playlistId, String songId, int userId) {
 
     }
 
@@ -95,11 +162,34 @@ public class JdbcPlaylistDao implements PlaylistDao{
 
     }
 
+    private Song mapRowToSong(SqlRowSet rs) {
+        Song song = new Song();
+
+        song.setId(rs.getString("song_id"));
+        song.setName(rs.getString("title"));
+        try {
+            song.setRating(rs.getInt("song_rating"));
+        } catch (Exception e) {
+            song.setRating(0);
+        }
+        try {
+            song.setVotes(rs.getInt("votes"));
+        } catch (Exception e) {
+            song.setVotes(0);
+        }
+        song.setPreview(rs.getString("preview"));
+        song.setSpotifyUri(rs.getString("spotify_link"));
+
+        return song;
+    }
+
     private Playlist mapRowToPlaylist(SqlRowSet rs){
         Playlist playlist = new Playlist();
+
         playlist.setName(rs.getString("name"));
         playlist.setDescription(rs.getString("description"));
         playlist.setPlaylistId(rs.getInt("playlist_id"));
+        playlist.setSpotifyId(rs.getString("spotify_id"));
 
         return playlist;
     }
@@ -134,11 +224,5 @@ public class JdbcPlaylistDao implements PlaylistDao{
             return true;
         }
             return false;
-
     }
-
-
-
-
-
 }
