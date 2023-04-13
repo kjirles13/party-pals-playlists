8a51edcc503da0d0c851f8586daa42f6390482b8
Blake Ford <blakeford96@gmail.com>
2023-04-13 15:24:52 -0400

    uhohhhh

--- vue/src/components/Playlist.vue
+++ vue/src/components/Playlist.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="Playlist">
         <!-- button, name and description for playlist -->
+
         <button v-on:click="getPlaylist">DISPLAY PLAYLISTS</button>
-        <div v-for="playlist in playlists" :key="playlist.id" class="playlist">
+        <!-- <div v-for="playlist in playlists" :key="playlist.id" class="playlist">
         <h1>Playlist Name:{{ playlist.name }}</h1>
         <p>Description:{{ playlist.description }}</p>
-        </div>
+        </div> -->
         <ul>
             <!-- songs in playlist -->
-            <li v-for="song in playlist.songs" :key="song.song_id">
+            <!-- <tr v-for="event in events" :key="event.event_id"> -->
+            <li v-for="playlist in playlists" :key="playlist.id">
                 <p>{{ song.title }}</p>
                 <p>Rating: {{ song.rating }} </p>
                 <p>Votes: {{ song.votes }} </p>
@@ -16,68 +18,27 @@
                 <button @click="dislike(song)">Dislike</button>
                 </li>
         </ul>
-        
     </div>
 </template>
 
 <script> 
 import axios from 'axios';
-import PlaylistService from '../services/PlaylistService'
 
-
-export default {
+export default{
     name: 'Playlist',
-    props: {
-        playlist: {
-            type: Object,
-            required: true
-        }
-    },
-        data() {
+    data() {
         return {
-            playlists: [{}],
+            playlist: [],
+        };
+    },
+    async created() {
+        try {
+            const response = await axios.get('/playlist');
+            this.events = response.data;
+        } catch (error) {
+            console.error(error);
         }
     },
-    mounted() {
-        axios.get(`http://localhost:9000/playlists/${this.$route.params.id}`)
-        .then(response => {
-            this.playlists = response.data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    },
-    methods: {
-        getPlaylist() {
-            PlaylistService.get('http://localhost:9000/playlists')
-            .then(response => {
-                this.playlists = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        like(song) {
-            song.likes++;
-            axios.put(`http://localhost:9000/songs/${song.song_id}`, song)
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        dislike(song){
-            song.dislikes--;
-            axios.put(`http://localhost:9000/songs/${song.song_id}`, song)
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
-    }
-}
 
+};
 </script>
--- vue/src/services/PlaylistService.js
+++ vue/src/services/PlaylistService.js
@@ -1,43 +1,85 @@
 import axios from 'axios';
 
+const baseURL = 'http://localhost:9000';
 
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+});
+const handleErrors = (error) => {
+    console.error(error);
+    throw error;
+};
 
+const getSong = (songId) => {
+    return axiosInstance
+    .get(`/songs/${songId}`)
+    .then((Response) => Response.data)
+    .catch(handleErrors);
+};
+const getAllSongs = () => {
+    return axiosInstance
+    .get('/events')
+    .then((Response) => Response)
+    .catch(handleErrors);
+};
+//     getSongs(principal) {
+//         return axios.get(`/songs?principal=${principal}`)
+//     },
+
+    
+//     addSong(song, principal) {
+//         return axios.post('/songs', song, {
+//             headers: {
+//                 Authorization: `Bearer ${localStorage.getItem('token')}`
+//             },
+//             params: {
+//                 principal: principal
+//             }
+//         })
+//     },
+//     deleteSong(songId, principal) {
+//         return axios.delete(`/songs/${songId}`, {
+//             headers: {
+//                 Authorization: `Bearer ${localStorage.getItem('token')}`
+//             },
+//             params: {
+//                 principal: principal
+//             }
+//         })
+//     },
+//     updateSong(playlistId, dto) {
+//         return axios.patch(`/songs/${playlistId}`, {
+//             genre: dto.genre,
+//             rating: dto.rating
+//         }, {
+//             headers: {
+//                 Authorization: `Bearer ${localStorage.getItem('token')}`
+//             }
+//         })
+//     },
+//     getPlaylist(playlistId) {
+//         return axios.get(`/playlists/${playlistId}`)
+//     },
+//     const getAllPlaylist() = () => {
+//         return axiosInstance
+//         .get('/events')
+//         .then((Response) => Response)
+//         .catch(handleErrors);
+//     };
+    
+ 
 
 export default {
-    getSongs(principal) {
-        return axios.get(`/songs?principal=${principal}`)
-    },
-    addSong(song, principal) {
-        return axios.post('/songs', song, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            },
-            params: {
-                principal: principal
-            }
-        })
-    },
-    deleteSong(songId, principal) {
-        return axios.delete(`/songs/${songId}`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            },
-            params: {
-                principal: principal
-            }
-        })
-    },
-    updateSong(playlistId, dto) {
-        return axios.patch(`/songs/${playlistId}`, {
-            genre: dto.genre,
-            rating: dto.rating
-        }, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-    },
-    getPlaylist(playlistId) {
-        return axios.get(`/playlists/${playlistId}`)
-    }
-}
+    // createSong,
+    // updateSong,
+    getSong,
+    getAllSongs,
+    // getSongsByUser,
+    // submitSong,
+    // voteOnSong,
+    // vetoSong,
+    // deleteSong,
+};
