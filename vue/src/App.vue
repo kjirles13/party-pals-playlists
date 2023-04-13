d8078cee6fec8515336e90816e8ffbcc2269f90a
Blake Ford <blakeford96@gmail.com>
2023-04-13 11:21:02 -0400

    songs

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,10 +2,12 @@
   <nav>
     <div id="app">
       <header>
-        <h1><img src="./images//DJ Performance Illustration Logo (11).png" class="title"/>Party Playlist Pal </h1>
+        <h1>
+          <img src="./images//DJ Performance Illustration Logo (11).png" class="title">
+        </h1>
+        <p1 style="align: center;">Party Playlist Pal</p1>
       </header>
       <div id="MainView">
-      
       <navigation class="navbar"/>
       <router-view class="main"/>
 
@@ -37,7 +39,7 @@ export default {
 .main {
   flex-grow: 1;
   margin-left: 115px;
-  margin-top: 100px;
+  margin-top: 80px;
   
 }
 .title {
@@ -48,58 +50,28 @@ export default {
   position: fixed;
   width: 100px;
   border-right-color: black 20px;
+  z-index: 1;
   
 }
 header {
   display: flex;
   position: fixed;
-  justify-content: space-between;
   align-items: center;
-  height: 80px; 
-  padding: 0 20px;
+  height: 80px;
+  width: 100%;
   background-color: rgb(145, 28, 28);
   text-align: center;
-  border: solid black 1px;
   top: 0px;
-
+  z-index: 1; /* set a higher z-index for the header */
+}
+p1{
+  font-family: sans-serif;
+  font-size: large;
 }
 img {
   margin-right: 10px;
   max-width: 150px;
 }
-
-/* .navigation {
-  position: absolute;
-  grid-column: 1/2;
-} */
-/* .content {
-  margin-left: 200px;
-  grid-column: 2/3;
-} */
-/* .item-0 {
-  grid-column: 1/2 !important;
-} */
-/* nav-bar {
-  grid-column: 1/2;
-  position: fixed;
-  background-color: #F2F2F2;
-  
-} */
-/* ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-} */
-/* li {
-  padding: 10px;
-} */
-/* a {
-  text-decoration: none;
-  color: #333;
-} */
-/* a:hover {
-  color: rgb(231, 219, 219);
-} */
 .angry-grid {
   display: grid;
   grid-template-rows: 1fr 1fr 1fr;
@@ -128,9 +100,4 @@ img {
   grid-row-end: 4; 
   grid-column-end: 6;
 }
-/* .grid-container {
-  display: grid;
-  grid: 1fr,
-} */
-
 </style>
--- vue/src/services/SongService.js
+++ vue/src/services/SongService.js
@@ -16,5 +16,4 @@ export default {
     deleteSong(id) {
         return axios.delete(`/songs/${id}`);
     }
-
 }
