01abd6b0f3ffbb669310e6b00264d011c0f05cdc
daniel pattison <danielpattison93@gmail.com>
2023-04-14 14:57:35 -0400

    banner width and about us component

---
+++ vue/src/components/AboutUs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>About Us</h1>
+    <p>Welcome to Party Playlist, the ultimate party planning app!</p>
+    <p>Our app is designed to help DJs manage their business by creating events, adding songs to their playlist, and assigning customers as hosts. Guests can vote on their favorite songs, ensuring that everyone has a great time at the party.</p>
+    <p>Our app is easy to use, and we offer a variety of features to make your party planning experience as smooth as possible. Sign up today to start planning your next party!</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'AboutUs',
+}
+</script>
+
+<style scoped>
+.about {
+    margin: 20px;
+    font-size: 26px;
+}
+</style>
