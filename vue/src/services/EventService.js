b3aadf5cdb0f600fc6e92429315da1aa8e026914
Blake Ford <blakeford96@gmail.com>
2023-04-16 19:40:11 -0400

    likes

--- vue/src/services/EventService.js
+++ vue/src/services/EventService.js
@@ -5,32 +5,26 @@ export default {
 getAllEvents() {
     return axios.get('/events');
 },
-
-// getEventById(eventId){
-//     return axios.get(`/events/${parseInt(eventId)}`)
-//   },
-
 getEventById(eventId){
-    return axios.get(`/events/${eventId}`).then((response) => {
-        return response.data;
-        
-    })
+  return axios.get(`/events/${eventId}`).then((response) => {
+  return response.data;
+  })
 },
-
 createEvent(event) {
-    return axios.post('/events', event);
-  },
-
-  updateEvent(eventInfo, eventId) {
-    return axios.put(`/events/${eventId}`, eventInfo);
-  },
-
-  deleteEvent(eventId) {
-    return axios.delete(`/events/${eventId}`);
+  return axios.post('/events', event);
 },
-
+updateEvent(eventInfo, eventId) {
+  return axios.put(`/events/${eventId}`, eventInfo);
+},
+deleteEvent(eventId) {
+  return axios.delete(`/events/${eventId}`);
+},
 addHostToEvent(eventId, hosts) {
-    return axios.put(`/events/${eventId}/hosts`, hosts);
-  }
+  return axios.put(`/events/${eventId}/hosts`, hosts);
+},
+saveLikes(song) {
+  return axios.put(`/events/playlists/songs/${song.song_id}`, { votes: song.likes });
+},
 
 }
+
