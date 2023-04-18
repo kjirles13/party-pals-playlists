ef9aee80cda35e68613bf963dfb0103d23dd7a32
Kit Jirles <kjirles13@gmail.com>
2023-04-18 12:51:19 -0400

    Work on searching spotify in MyEvents

--- vue/src/views/MyEvents.vue
+++ vue/src/views/MyEvents.vue
@@ -1,35 +1,16 @@
 <template>
   <div class="Events">
     <h1>My Events</h1>
-    <myEvents />
     <events />
-
-  <h1>My Songs</h1>
-  <div id="song-container">
-    <div>
-      <h2 style="text-align: center;">Current Playlist</h2>
-    <song-display
-     v-for="song in this.songs"
-        :key="song.song_id"
-        :song="song"/>
-    </div>
-    <div>
-      <h2 style="text-align: center;">Add Songs</h2>
-      <spotify-search />
-    </div>
-
-  </div>
-    
+    <myEvents />  
   </div>
 </template>
 
 <script>
 import myEvents from "../components/DJEventView.vue";
 import events from "../components/EventList.vue";
-import songService from "../services/SongService";
+// import songService from "../services/SongService";
 import eventService from "../services/EventService";
-import songDisplay from "../components/SongDisplay.vue";
-import SpotifySearch from "../components/DJSearchSong.vue"
 
 export default {
   name: "MyEvents",
@@ -37,19 +18,14 @@ export default {
   components: {
     events,
     myEvents,
-    songDisplay,
-    SpotifySearch
   },
-  // mounted() {
-  //   document.title = "Events";
-  // },
   data() {
     return {
       songs: [],
     };
   },
   created() {
-    this.getSongs();
+    // this.getSongs();
     this.getEvents();
   },
 methods: {
@@ -67,13 +43,13 @@ methods: {
         }});
       this.$store.commit("SET_EVENTS", filteredEvents);
     },
-    getSongs() {
-      songService.getSongs(this.$store.state.user.username).then((response) => {
-      if (response.status == 200) {
-        this.songs = response.data;
-      }
-    });
-    }
+    // getSongs() {
+    //   songService.getSongs(this.$store.state.user.username).then((response) => {
+    //   if (response.status == 200) {
+    //     this.songs = response.data;
+    //   }
+    // });
+    // }
 }
   
 };
@@ -82,9 +58,5 @@ methods: {
 <style scoped>
 h1 {
   text-align: center;
-}
-#song-container {
-  display: flex;
-  justify-content: space-evenly;
 }
 </style>
