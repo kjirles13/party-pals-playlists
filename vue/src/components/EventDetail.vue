1120bcb1aef31dffa4b014eb598621d5d68ded85
daniel pattison <danielpattison93@gmail.com>
2023-04-19 16:09:21 -0400

    fixed edit event and worked on styling event list

--- vue/src/components/EventDetail.vue
+++ vue/src/components/EventDetail.vue
@@ -208,7 +208,7 @@ export default {
         .catch((error) => {
           console.log("Error updating event:", error);
         });
-        this.isEditing();
+        this.isEditing = false;
     },
     deleteHost(hostName) {
       eventService.removeHostFromEvent(this.event.id, hostName).then(() => {
